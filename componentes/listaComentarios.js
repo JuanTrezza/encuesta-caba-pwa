62abf56499230e7a8ec637521bd1c6967726dfcb
Juan Moreno Trezza <jgmorenotrezza@gmail.com>
2025-07-17 18:33:32 -0300

    correciones 2

--- componentes/listaComentarios.js
+++ componentes/listaComentarios.js
@@ -1,7 +1,7 @@
 import { traerComentarios } from '../db.js';
-
 export async function mostrarComentarios(categoria, container) {
   const comentarios = await traerComentarios(categoria);
+  console.log("Comentarios desde Firestore:", comentarios); // <-- AGREGÁ ESTO
   container.innerHTML = comentarios.map(c => `
     <div class="comentario">
       <strong class="puntuacion">${"★".repeat(c.calificacion)}</strong><br/>
